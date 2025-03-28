03776bdc7dd6d7aa23a3635540b6d63c0150673e
Minh Duong <johncegom203@gmail.com>
2025-03-28 14:03:14 +0700

    Change firebase service to use env variables

--- src/services/firebase/Firebase.tsx
+++ src/services/firebase/Firebase.tsx
@@ -7,13 +7,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBTHoYNraumNcT3bfgwqSrPDrIUxtP4GUI",
-  authDomain: "my-portfolio-3d706.firebaseapp.com",
-  projectId: "my-portfolio-3d706",
-  storageBucket: "my-portfolio-3d706.appspot.com",
-  messagingSenderId: "159615989054",
-  appId: "1:159615989054:web:654193c75e4d00e6b7e86f",
-  measurementId: "G-V13X00TCF8",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
